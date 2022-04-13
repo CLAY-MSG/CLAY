afd0801c7f75d8342f7f1cfa1f516ff7b5f34f1e
CLAY <mishuaiguang@gmail.com>
2022-04-13 11:20:44 +0800

    构建项目

---
+++ clay-web/src/main/java/xyz/sgmi/clay/ClayApplication.java
@@ -0,0 +1,14 @@
+package xyz.sgmi.clay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author MSG
+ */
+@SpringBootApplication
+public class ClayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClayApplication.class,args);
+    }
+}
